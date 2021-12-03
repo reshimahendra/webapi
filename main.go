eebb44543194d9a615de9f5b76b7aa7857f664a0
Reshi Mahendra <reshimahendra@gmail.com>
2021-12-03 15:24:04 +0800

    Between url params and query param

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ func main() {
     router.SetTrustedProxies([]string{"192.168.43.26"})
 
     router.GET("/hello", HelloHandler) 
+    router.GET("/book/:id/:title", BookDetailHandler)
+    router.GET("/search", SearchQueryHandler)
     router.GET("/", RootHandler)
+
     router.Run(":3888")
 }
 
@@ -28,4 +31,18 @@ func HelloHandler(c *gin.Context) {
         "bio": "An Internet marketter",
         "content": "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
     })   
+}
+
+func BookDetailHandler(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+        "id" : c.Param("id"),
+        "title": c.Param("title"),
+    })
+}
+
+func SearchQueryHandler(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+        "q": c.Query("q"),
+        "price": c.Query("price"),
+    })
 }
