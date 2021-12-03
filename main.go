0e03e03884dd580d8836b3052d40e576bb17e5fc
Reshi BSD <reshimahendra@gmail.com>
2021-12-04 06:18:38 +0800

    POST JSON data in golang with Curl

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,6 +13,7 @@ func main() {
 
     router.GET("/hello", HelloHandler) 
     router.GET("/book/:id/:title", BookDetailHandler)
+    router.POST("/books", BookPostHandler)
     router.GET("/search", SearchQueryHandler)
     router.GET("/", RootHandler)
 
@@ -23,6 +25,12 @@ func RootHandler(c *gin.Context) {
         "name": "Reshi Mahendra",
         "Bio": "Spearfisher Man",
     })
+}
+
+func CheckError(e error) {
+    if e != nil {
+        log.Panic(e)
+    }
 }
 
 func HelloHandler(c *gin.Context) {
@@ -44,5 +52,27 @@ func SearchQueryHandler(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
         "q": c.Query("q"),
         "price": c.Query("price"),
+    })
+}
+
+type BookInput struct{
+    Title string
+    Price int
+    SubTitle string `json:"sub_title"`      // Jika nama field di 'Struct' dan nama field JSON beda
+                                            // maka secara implisit harus dideklarasikan setelah
+                                            // deklarasi field di struct sehingga saat dipanggil melalui
+                                            // context "ShouldBindJSON", perbedaan nama field
+                                            // bisa disinkronisasi
+}
+
+func BookPostHandler(c *gin.Context) {
+    var bookInput BookInput
+    err := c.ShouldBindJSON(&bookInput)
+    CheckError(err)
+
+    c.JSON(http.StatusOK, gin.H{
+        "title": bookInput.Title,
+        "price": bookInput.Price,
+        "sub_title": bookInput.SubTitle,    // input nama field "struct" dan field "JSON" berbeda
     })
 }
