24af132e207a75feef5c18535cd4ccc5e10ad7a4
Reshi Mahendra <reshimahendra@gmail.com>
2021-12-04 14:27:58 +0800

    Api versioning and project restructuring

--- main.go
+++ main.go
@@ -1,84 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
+    hdl "webapi/handler"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func main() {
     router := gin.Default()
     router.SetTrustedProxies([]string{"192.168.43.26"})
 
-    router.GET("/hello", HelloHandler) 
-    router.GET("/book/:id/:title", BookDetailHandler)
-    router.POST("/books", BookPostHandler)
-    router.GET("/search", SearchQueryHandler)
-    router.GET("/", RootHandler)
+    // membuat api versioning dengan nama v1 (api version 1)
+    v1 := router.Group("/v1")
+
+    v1.GET("/hello", hdl.HelloHandler) 
+    v1.GET("/book/:id/:title", hdl.BookDetailHandler)
+    v1.POST("/books", hdl.BookPostHandler)
+    v1.GET("/search", hdl.SearchQueryHandler)
+    v1.GET("/", hdl.RootHandler)
+
+    v2 := router.Group("/v2")
+    v2.POST("/books", hdl.BookPostHandler2)
 
     router.Run(":3888")
 }
 
-func RootHandler(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-        "name": "Reshi Mahendra",
-        "Bio": "Spearfisher Man",
-    })
-}
-
-
-func HelloHandler(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-        "name": "Reshi",
-        "bio": "An Internet marketter",
-        "content": "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-    })
-}
-
-func BookDetailHandler(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-        "id": c.Param("id"),
-        "name" : c.Param("name"),
-        "title": c.Param("title"),
-    })
-}
-
-func SearchQueryHandler(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-        "q": c.Query("q"),
-        "price": c.Query("price"),
-    })
-}
-
-type BookInput struct{
-    Title string `json:"title" binding:"required"`
-    Price json.Number `json:"price" binding:"required,number,gt=0"`
-    SubTitle string `json:"sub_title" binding:"required"`
-}
-
-func BookPostHandler(c *gin.Context) {
-    var bookInput BookInput
-    err := c.ShouldBindJSON(&bookInput)
-
-    eMsg := []string{}
-    if err != nil {
-        for _, e := range err.(validator.ValidationErrors) {
-            msg := fmt.Sprintf("Error on field '%s', condition: '%s'.", e.Field(), e.ActualTag())
-            eMsg = append(eMsg, msg)
-        }
-
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": eMsg,
-        })
-        return
-    }   
-
-    c.JSON(http.StatusOK, gin.H{
-        "title": bookInput.Title,
-        "price": bookInput.Price,
-        "sub_title": bookInput.SubTitle,
-    })
-}
